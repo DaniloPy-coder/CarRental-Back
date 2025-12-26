e0531475af5c9e6cdbf21aa09fb8cb0075f02b27
Danilo <danilodevsp@gmail.com>
2025-12-26 14:41:55 -0300

    feat

--- src/services/user/ChangeRoleService.ts
+++ src/services/user/ChangeRoleService.ts
@@ -1,14 +1,11 @@
 import prismaClient from '../../prisma'
-
 export class ChangeUserRoleService {
     async execute(userId: string) {
         const user = await prismaClient.user.findUnique({
             where: { id: userId },
         })
 
-        if (!user) {
-            throw new Error('Usuário não encontrado')
-        }
+        if (!user) throw new Error('Usuário não encontrado')
 
         const updatedUser = await prismaClient.user.update({
             where: { id: userId },
