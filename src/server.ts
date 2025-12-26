4986fd9b1a9790e262b92a7a2da67abf23f579f9
Danilo <danilodevsp@gmail.com>
2025-12-26 17:59:30 -0300

    fix: fix a route front

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://car-rental-iu861vhnx-danilodevs-projects-8f9b933b.vercel.app"
+        "https://car-rental-taupe-two.vercel.app"
     ],
     credentials: true
 }));
