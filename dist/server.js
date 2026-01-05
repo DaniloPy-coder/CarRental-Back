49bfe392eadbec4300e3550ab1a15d0dccd4550f
Danilo <danilodevsp@gmail.com>
2026-01-05 18:46:50 -0300

    corrigindo as rotas

--- dist/server.js
+++ dist/server.js
@@ -6,24 +6,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
-const path_1 = __importDefault(require("path"));
+const multer_1 = __importDefault(require("multer"));
+const multer_2 = __importDefault(require("./config/multer"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
     origin: [
         "http://localhost:5173",
-        "https://car-rental-taupe-two.vercel.app",
+        "https://car-rental-taupe-two.vercel.app"
     ],
     credentials: true,
 }));
-const __dirnameFix = path_1.default.resolve();
-app.use(express_1.default.static(path_1.default.join(__dirnameFix, "..", "frontend", "dist")));
+const upload = (0, multer_1.default)(multer_2.default.upload());
 app.use("/", routes_1.default);
-app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirnameFix, "..", "frontend", "dist", "index.html"));
-});
-// 🔹 ERROR HANDLER
 app.use((err, req, res, next) => {
-    return res.status(400).json({ error: err.message });
+    if (err instanceof Error) {
+        res.status(400).json({
+            error: err.message,
+        });
+    }
+    else {
+        res.status(500).json({
+            status: "error",
+            message: "Internal server error.",
+        });
+    }
+});
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 exports.default = app;
--- src/server.ts
+++ src/server.ts
@@ -3,41 +3,41 @@ import cors from "cors";
 import router from "./routes";
 import multer from "multer";
 import uploadConfig from "./config/multer";
-import path from "path";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://car-rental-taupe-two.vercel.app",
+            "https://car-rental-taupe-two.vercel.app"
         ],
         credentials: true,
     })
 );
 
-const __dirnameFix = path.resolve();
 
-app.use(
-    express.static(
-        path.join(__dirnameFix, "..", "frontend", "dist")
-    )
-);
+const upload = multer(uploadConfig.upload());
 
 app.use("/", router);
 
-app.get("*", (req: Request, res: Response) => {
-    res.sendFile(
-        path.join(__dirnameFix, "..", "frontend", "dist", "index.html")
-    );
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        res.status(400).json({
+            error: err.message,
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "Internal server error.",
+        });
+    }
 });
 
-// 🔹 ERROR HANDLER
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    return res.status(400).json({ error: err.message });
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
